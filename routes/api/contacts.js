8bb9a8d673815ed86262985ef86e2e0454d9ccde
anfdrva <angelina0403@ukr.net>
2023-12-11 22:01:39 +0200

    first

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,22 +2,22 @@ const express = require('express');
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody, isValidId } = require('../../middlewares');
+const { validateBody, isValidId, authenticate } = require('../../middlewares');
 
 const { schemas } = require('../../models/contact');
 
 const router = express.Router();
 
-router.get('/', ctrl.getAll);
+router.get('/', authenticate, ctrl.getAll);
 
-router.get('/:contactId', isValidId, ctrl.getById);
+router.get('/:contactId', authenticate, isValidId, ctrl.getById);
 
-router.post('/', ctrl.add);
+router.post('/', authenticate, ctrl.add);
 
-router.delete('/:contactId', isValidId, ctrl.deleteById);
+router.delete('/:contactId', authenticate, isValidId, ctrl.deleteById);
 
-router.put('/:contactId', isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put('/:contactId', authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.patch('/:contactId/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateStatusContact)
+router.patch('/:contactId/favorite', authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateStatusContact)
 
 module.exports = router;
